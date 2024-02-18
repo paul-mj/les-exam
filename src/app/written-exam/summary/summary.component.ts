6b588fc8e0fa06f011537d30254609a255fc48bf
sajin-accinge <an.sajinsatheesan@gmail.com>
2024-02-18 14:47:54 +0400

    changes

--- src/app/written-exam/summary/summary.component.ts
+++ src/app/written-exam/summary/summary.component.ts
@@ -1,13 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-summary',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+    selector: 'app-summary',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatExpansionModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,],
+    templateUrl: './summary.component.html',
+    styleUrl: './summary.component.scss'
 })
+
+
 export class SummaryComponent {
+
+    @ViewChild(MatAccordion) accordion!: MatAccordion;
+    groupedData: any[] = [];
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialogRef: MatDialogRef<SummaryComponent>,
+
+    ) {
+        this.groupDataByCategory(data);
+        console.log(this.groupedData);
+    }
+
+    groupDataByCategory(data: any) {
+        const groupedDataMap: any = {};
+        data.forEach((item: any) => {
+            const categoryId = item.category.CATEGORY_ID;
+            if (!groupedDataMap[categoryId]) {
+                groupedDataMap[categoryId] = [];
+            }
+            groupedDataMap[categoryId].push(item);
+        });
+        this.groupedData = Object.values(groupedDataMap);
+    }
+
+
+    onClickToQuestion(question: any) {
+        this.dialogRef.close({question, close: true, isComplete: false})
+    }
 
 }
