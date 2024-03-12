6e8bc3dd48328d92fde7e87b9279778c070b5526
pujaanil <pujaanil007@gmail.com>
2024-03-12 14:25:37 +0530

    result

--- src/app/written-exam/result/result.component.ts
+++ src/app/written-exam/result/result.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { INewExamOrRetest, ResultData } from '../../core/interfaces/exam-interface';
+
 
 @Component({
     selector: 'app-result',
     standalone: true,
     imports: [CommonModule],
+    providers: [DatePipe],
     templateUrl: './result.component.html',
     styleUrl: './result.component.scss'
 })
